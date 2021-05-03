d0c9accf498db5e11d1c2eed08b7af689029f11d
eldarYarden <eldar.yarden@ncr.com>
2021-05-03 12:18:29 +0300

    Add project files.

---
+++ Controllers/GifController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Model;
+using NSwag.Annotations;
+using System.Net;
+
+namespace FetchGiphyApp.Controllers
+{
+    [Route("GifSearcher")]
+    public class GifController : Controller
+    {
+        private readonly ILogger<GifController> _logger;
+        private readonly IGifService _gifService;
+        public GifController(ILogger<GifController> logger, IGifService gifService)
+        {
+            _logger = logger;
+            _gifService = gifService;
+        }
+
+        [HttpGet("/Trending")]
+        [SwaggerResponse(HttpStatusCode.OK, typeof(GifResponse), Description = "The Gif Response inclueds images data")]
+
+        public ActionResult GetTrending(int limit)
+        {
+            //var trendingConfig = GetTrendingConfig();
+            return Ok(_gifService.GetTrending(limit));
+        }
+
+        [HttpGet("/Search/{queryTerm}")]
+        [SwaggerResponse(HttpStatusCode.OK, typeof(GifResponse), Description = "The Gif Response inclueds images data")]
+
+        public ActionResult Search(string queryTerm,int? limit)
+        {
+            //var trendingConfig = GetTrendingConfig();
+            return Ok(_gifService.Search(queryTerm,limit));
+        }
+
+        //private ITrendingConfiguration GetTrendingConfig()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,64 @@
+using Components;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Model;
+using System;
+
+namespace FetchGiphyApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FetchGiphyApp", Version = "v1" });
+            });
+            services.AddTransient<IGifService>(CreateGifService);
+            services.AddTransient<ICashService<GifResponse>>(p=>new CashService<GifResponse>());
+        }
+
+        //TODO: use factory anf register each implementation in its project.
+        private IGifService CreateGifService(IServiceProvider provider)
+        {
+            return new GifService(provider.GetService<ICashService<GifResponse>>());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FetchGiphyApp v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+    }
+}
